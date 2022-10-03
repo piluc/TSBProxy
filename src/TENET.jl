369dcb1275086daf227d184ed1876464e60a4ebd
Antonio <antonio@pop-os.localdomain>
2022-10-03 18:56:51 +0200

    added ego

--- src/TENET.jl
+++ src/TENET.jl
@@ -8,5 +8,6 @@ include("centralities/temporal_shortest_betweenness.jl")
 include("centralities/onbra.jl")
 include("centralities/pass_through_degree.jl")
 include("centralities/prefix_foremost_betweenness.jl")
+include("centralities/ego_shortest_betweenness.jl")
 include("centralities/utilities.jl")
 end;
